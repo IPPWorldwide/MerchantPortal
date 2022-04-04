3bf9d893588e4da7ed438c55df6d6a701de9b193
Mathias Gajhede <mag@venturecapitals.dk>
2022-04-04 16:14:36 +0200

    Update head.php

--- theme/standard/head.php
+++ theme/standard/head.php
@@ -40,13 +40,9 @@ function head() {
     
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
   <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
-  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
   <div class="navbar-nav">
     <div class="nav-item text-nowrap">
-      <a class="nav-link px-3" href="#">Sign out</a>
+      <a class="nav-link px-3" href="/logout.php">Sign out</a>
     </div>
   </div>
 </header>
@@ -66,6 +62,18 @@ function head() {
                 <a class="nav-link active" aria-current="page" href="/charts/">
                     <span data-feather="home"></span>
                     Charts
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/subscriptions/">
+                    <span data-feather="home"></span>
+                    Subscriptions
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/virtual_terminal/">
+                    <span data-feather="home"></span>
+                    Virtual Terminal
                 </a>
             </li>
             <li class="nav-item">
