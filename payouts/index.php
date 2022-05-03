7efcb16a4b1991d91deb94c81f5d19bebf950dbe
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-03 12:30:05 +0200

    Table for Payout

--- payouts/index.php
+++ payouts/index.php
@@ -4,5 +4,18 @@ include("../base.php");
 echo head();
 ?>
       <h2>Payouts</h2>
+    <table class="table table-striped table-sm">
+        <thead>
+        <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Gross Volume</th>
+            <th scope="col">Calculated Fees</th>
+            <th scope="col">Settlement</th>
+            <th scope="col">Settled</th>
+        </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
 <?php
 echo foot();
