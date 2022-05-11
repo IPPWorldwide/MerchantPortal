f7e503cced1f1a8d61d91070156c2db90e916933
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-12 00:51:10 +0200

    Update foot.php

--- theme/standard/partner/foot.php
+++ theme/standard/partner/foot.php
@@ -1,5 +1,20 @@
 <?php
 function foot() {
+    global $inline_css, $inline_script,$load_script,$plugins;
+    $css = "";
+    $script = "";
+    foreach($inline_css as $value) {
+        $css .= "<style>".$value."</style>";
+    }
+    foreach($inline_script as $value) {
+        $script .= "<script>".$value."</script>";
+    }
+    foreach($load_script as $value) {
+        $script .= "<script src='".$value."'></script>";
+    }
+    foreach($plugins->hook_footer as $value) {
+        $script .= $value;
+    }
     return "
     </main>
   </div>
@@ -11,6 +26,7 @@ function foot() {
 <script src=\"/assets/dist/js/bootstrap.bundle.min.js\"></script>
 
       <script src=\"https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js\" integrity=\"sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE\" crossorigin=\"anonymous\"></script><script src=\"https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js\" integrity=\"sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha\" crossorigin=\"anonymous\"></script><script src=\"dashboard.js\"></script>
+    $css $script
   </body>
 </html>
 ";
