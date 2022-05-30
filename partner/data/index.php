698461bf3fd49cdef10452055f857369fbd98ec1
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-30 23:54:13 +0200

    Language

--- partner/data/index.php
+++ partner/data/index.php
@@ -7,58 +7,61 @@ if(isset($REQ["submit"])) {
 $partner_data = $partner->PartnerData();
 
 echo head();
-?>
+echo '
         <form action="?" method="POST" class="form">
-            <h2>Partner Data</h2>
+            <h2>'.$lang["PARTNER"]["DATA"]["HEADER"].'</h2>
             <div class="row row-cols-md-3 mb-3">
-                <div class="col themed-grid-col">Partner ID:<br /><input name="id" class="form-control" value="<?php echo $partner_data->id; ?>" readonly></div>
-                <div class="col themed-grid-col">Key 1:<br /><input name="security[key1]" class="form-control" value="<?php echo $partner_data->security->key1; ?>"></div>
-                <div class="col themed-grid-col">Key 2:<br /><input name="security[key2]" class="form-control" value="<?php echo $partner_data->security->key2; ?>"></div>
+                <div class="col themed-grid-col">'.$lang["PARTNER"]["DATA"]["PARTNER_ID"].'<br /><input name="id" class="form-control" value="'.$partner_data->id.'" readonly></div>
+                <div class="col themed-grid-col">'.$lang["PARTNER"]["DATA"]["KEY_1"].'<br /><input name="security[key1]" class="form-control" value="'.$partner_data->security->key1.'"></div>
+                <div class="col themed-grid-col">'.$lang["PARTNER"]["DATA"]["KEY_2"].'<br /><input name="security[key2]" class="form-control" value="'.$partner_data->security->key2.'"></div>
             </div>
-            <h2>Partner Details</h2>
+            <h2>'.$lang["PARTNER"]["DATA"]["DETAILS"].'</h2>
             <div class="row row-cols-md-3 mb-3">
-                <div class="col themed-grid-col">Partner Name:<br /><input name="meta[name]" class="form-control" value="<?php echo isset($partner_data->meta_data->name) ? $partner_data->meta_data->name : ""; ?>"></div>
+                <div class="col themed-grid-col">'.$lang["PARTNER"]["DATA"]["NAME"].'<br /><input name="meta[name]" class="form-control" value="'; echo $partner_data->meta_data->name ?: ""; echo '"></div>
             </div>
             <div class="row row-cols-md-3 mb-3">
-                <div class="col themed-grid-col">Country:<br />
+                <div class="col themed-grid-col">'.$lang["PARTNER"]["DATA"]["COUNTRY"].'<br />
                     <select name="meta[country]" class="form-control" >
-                        <?php foreach($partner->ListCountry() as $key=>$value) {
+                        ';
+                        foreach($partner->ListCountry() as $key=>$value) {
                             echo "<option value='".$value->id."' ";
                             echo (isset($partner_data->country->id) && $partner_data->country->id == $value->id) ? "selected" : "";
                             echo ">".$value->name."</option>";
-                        } ?>
+                        }
+                        echo '
                     </select>
                     </div>
             </div>
-            <h2>Partner Invoices</h2>
+            <h2>'.$lang["PARTNER"]["DATA"]["PARTNER_INVOICES"].'</h2>
             <div class="row row-cols-md-12 mb-12">
-                <div class="col themed-grid-col"Invoice text:<br /><input name="meta[invoicetext]" class="form-control" value="<?php echo isset($partner_data->meta_data->meta->invoicetext) ? $partner_data->meta_data->meta->invoicetext : ""; ?>">
+                <div class="col themed-grid-col"'.$lang["PARTNER"]["DATA"]["PAYMENT_SLIP"].'<br /><input name="meta[invoicetext]" class="form-control" value="'; echo $partner_data->meta_data->meta->invoicetext ?: ""; echo '">
             </div>
             <div class="row row-cols-md-2 mb-2">
                 <div class="col themed-grid-col">
-                    <h2>Acquirers</h2>
+                    <h2>'.$lang["PARTNER"]["DATA"]["ACQUIRERS"].'</h2>
                     <table class="table v-middle p-0 m-0 box" data-plugin="dataTable">
                         <thead>
                         <tr>
-                            <th>Name</th>
-                            <th>ID</th>
-                            <th>Website</th>
+                            <th>'.$lang["PARTNER"]["DATA"]["ACQUIRERS_NAME"].'</th>
+                            <th>'.$lang["PARTNER"]["DATA"]["ACQUIRERS_ID"].'</th>
+                            <th>'.$lang["PARTNER"]["DATA"]["ACQUIRERS_WEBSITE"].'</th>
                         </tr>
                         </thead>
                         <tbody>
-                        <?php foreach($partner_data->acquirers as $key=>$value) {
+                        ';
+                        foreach($partner_data->acquirers as $key=>$value) {
                             echo "<tr><td>".$value->name."</td><td>".$value->id."</td><td></td><td><a href='".$value->url."' target='_BLANK'>".$value->url."</a></td></tr>";
-                        } ?>
+                        }
+                        echo '
                         </tbody>
                     </table>
                 </div>
             </div>
             <div class="row row-cols-md-1 mb-1">
                 <div class="d-flex justify-content-end">
-                    <button type="submit" name="submit" class="btn btn-primary mb-3">Save</button>
+                    <button type="submit" name="submit" class="btn btn-primary mb-3">'.$lang["PARTNER"]["DATA"]["SAVE"].'</button>
                 </div>
             </div>
         </form>
-<?php
-
+';
 echo foot();
