22aa7b1b59c5610003b7e893e6d5963592d34bf6
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-30 15:04:23 +0200

    Update ipp-config-sample.php

--- ipp-config-sample.php
+++ ipp-config-sample.php
@@ -3,11 +3,10 @@
 $IPP_CONFIG["GLOBAL_BASE_URL"] = "https://api.ippworldwide.com/";
 $IPP_CONFIG["ONBOARDING_BASE_URL"] = "https://onboarding.api.ippworldwide.com/";
 $IPP_CONFIG["PORTAL_URL"] = "httpS://merchantportal.ippewa.com/";
-$IPP_CONFIG["domain_id"] = "";
-$IPP_CONFIG["partner_id"] = "";
-$IPP_CONFIG["partner_key1"] = "";
-$IPP_CONFIG["partner_key2"] = "";
-$IPP_CONFIG["git_user"] = "";
-$IPP_CONFIG["git_token"] = "";
-$IPP_CONFIG["version"] = "0.1.5";
+$IPP_CONFIG["DOMAIN_ID"] = "";
+$IPP_CONFIG["PARTNER_ID"] = "";
+$IPP_CONFIG["PARTNER_KEY1"] = "";
+$IPP_CONFIG["PARTNER_KEY2"] = "";
+$IPP_CONFIG["VERSION"] = "1.0.0";
 $IPP_CONFIG["THEME"] = "standard";
+$IPP_CONFIG["MENU"] = "{'PARTNER':{'dashboard':'Dashboard','companies':'Companies','invoices':'Companies Invoices','invoices\/plans\/':'Companies Recurring Plans','data':'Partner Data','users':'Users','plugins':'Plugins','communications':'Outbound Communications','menus':'Menu Administration'},'COMPANY':{'dashboard':'Dashboard','charts':'Charts','subscriptions':'Cardholder Subscriptions','virtual_terminal':'Virtual Terminal','merchant_data':'Merchant Data','onboarding':'Onboarding','payouts':'Payouts','users':'Users','disputes':'Disputes','payment_links':'Payment Links','invoices':'Invoices'}}";
