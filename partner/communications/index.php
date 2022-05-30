fc6476b05eaf60b7f84ec133b99b11183aa5f6a5
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-31 00:05:08 +0200

    Update index.php

--- partner/communications/index.php
+++ partner/communications/index.php
@@ -50,40 +50,6 @@ echo head();
         </table>
       </div>
 
-    <div class="modal fade" id="passwordModal" tabindex="-1" role="dialog" aria-labelledby="passwordModalTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <input type="hidden" name="company_id" id="company-id" readonly>
-                        <input type="hidden" name="user_id" id="user-id" readonly>
-                        <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Username:</label>
-                            <input type="text" class="form-control"  name="username" id="username" readonly>
-                        </div>
-                        <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Password:</label>
-                            <input type="password" class="form-control checkPasswordUser" name="password" id="password">
-                        </div>
-                        <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Repeat Password:</label>
-                            <input type="password" class="form-control checkPasswordUser" id="repeat-password">
-                            <small id="PasswordRequirements">Password did not contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character</small>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary closeModal">Close</button>
-                    <button type="button" class="btn btn-primary confirm" disabled>Change password</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
 <?php
 
 echo foot();
