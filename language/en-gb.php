d92a68df6435506bcd5091b53e17b10a78300bad
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-30 23:15:08 +0200

    ENG Translation

--- language/en-gb.php
+++ language/en-gb.php
@@ -7,3 +7,23 @@ $lang["LOGIN"]["AS_PAYFAC"] = "Login as Payfac";
 $lang["LOGIN"]["SIGN_IN"] = "Sign in";
 
 
+$lang["PARTNER"]["COMPANIES"]["HEADER"] = "Companies";
+$lang["PARTNER"]["COMPANIES"]["ADD_NEW"] = "Add new Company";
+$lang["PARTNER"]["COMPANIES"]["INFO"] = "Info";
+$lang["PARTNER"]["COMPANIES"]["ONBOARDING"] = "Onboarding";
+$lang["PARTNER"]["COMPANIES"]["RESET_PASSWORD"] = "Reset Password";
+$lang["PARTNER"]["COMPANIES"]["CLOSE_ACCOUNT"] = "Close Account";
+$lang["PARTNER"]["COMPANIES"]["COMPANY_ID"] = "Company ID";
+$lang["PARTNER"]["COMPANIES"]["SECRET_KEY"] = "Secret Payment Key";
+$lang["PARTNER"]["COMPANIES"]["COMPANY_NAME"] = "Company Name";
+$lang["PARTNER"]["COMPANIES"]["DOMAINS"] = "Domains";
+$lang["PARTNER"]["COMPANIES"]["LOGIN"] = "Login";
+$lang["PARTNER"]["COMPANIES"]["UNPAID_INVOICES"] = "Unpaid invoices";
+$lang["PARTNER"]["COMPANIES"]["TOTAL_INVOICES"] = "Total Invoices";
+$lang["PARTNER"]["COMPANIES"]["STATUS"] = "Status";
+$lang["PARTNER"]["COMPANIES"]["MODAL_USERNAME"] = "Username:";
+$lang["PARTNER"]["COMPANIES"]["MODAL_PASSWORD"] = "Password:";
+$lang["PARTNER"]["COMPANIES"]["MODAL_REPEAT_PASSWORD"] = "Repeat Password:";
+$lang["PARTNER"]["COMPANIES"]["MODAL_PASSWORD_DESCRIPTION"] = "Password must contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character";
+$lang["PARTNER"]["COMPANIES"]["MODAL_CLOSE"] = "Close";
+$lang["PARTNER"]["COMPANIES"]["MODAL_SUBMIT_BTN"] = "Change password";
--- partner/companies/index.php
+++ partner/companies/index.php
@@ -13,32 +13,31 @@ if(isset($REQ["close"])) {
 
 $companies = $partner->ListCompany();
 echo head();
-?>
+echo '
     <div class="row">
         <div class="col-6">
-            <h2>Companies</h2>
+            <h2>'.$lang["PARTNER"]["COMPANIES"]["HEADER"].'</h2>
         </div>
         <div class="col-6 text-end">
-            <a class="btn btn-success" href="add.php">Add new Company</a>
+            <a class="btn btn-success" href="add.php">'.$lang["PARTNER"]["COMPANIES"]["ADD_NEW"].'</a>
         </div>
     </div>
       <div class="table-responsive">
         <table class="table table-striped table-sm">
           <thead>
             <tr>
-              <th scope="col">Company ID</th>
-              <th scope="col">Secret Key 2</th>
-              <th scope="col">Company Name</th>
-              <th scope="col">Domains</th>
-              <th scope="col">Login</th>
-              <th scope="col">Unpaid invoices</th>
-              <th scope="col">Total invoices</th>
-              <th scope="col">Status</th>
+              <th scope="col">'.$lang["PARTNER"]["COMPANIES"]["COMPANY_ID"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["COMPANIES"]["SECRET_KEY"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["COMPANIES"]["COMPANY_NAME"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["COMPANIES"]["DOMAINS"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["COMPANIES"]["LOGIN"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["COMPANIES"]["UNPAID_INVOICES"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["COMPANIES"]["TOTAL_INVOICES"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["COMPANIES"]["STATUS"].'</th>
             </tr>
           </thead>
           <tbody>
-          <?php
-          echo "<tr>";
+          <tr>';
           foreach($companies as $value) {
               $user = reset($value->users);
               echo "
@@ -53,51 +52,49 @@ echo head();
               <td>".$user->username."</td>
               <td>".$value->invoices->overdue."</td>
               <td>".$value->invoices->total."</td>
-              <td><a href='/partner/companies/details.php?id=".$value->id."' class='btn btn-dark'>Info</a></td>
+              <td><a href='/partner/companies/details.php?id=".$value->id."' class='btn btn-dark'>".$lang["PARTNER"]["COMPANIES"]["INFO"]."</a></td>
               <td>
-              <button type='button' class='btn btn-info ResetPasswordModal' data-companyname='".$value->name."' data-company='".$value->id."' data-id='".$user->id."' data-username='".$user->username."'>Reset Password</button></td>
-              <td><a href='/partner/companies/?close=1&company_id=".$value->id."' class='btn btn-warning'>Close Account</a></td>
+              <button type='button' class='btn btn-info ResetPasswordModal' data-companyname='".$value->name."' data-company='".$value->id."' data-id='".$user->id."' data-username='".$user->username."'>".$lang["PARTNER"]["COMPANIES"]["RESET_PASSWORD"]."</button></td>
+              <td><a href='/partner/companies/?close=1&company_id=".$value->id."' class='btn btn-warning'>".$lang["PARTNER"]["COMPANIES"]["CLOSE_ACCOUNT"]."</a></td>
             </tr>";
           }
           ?>
           </tbody>
         </table>
       </div>
-
+<?php
+echo '
     <div class="modal fade" id="passwordModal" tabindex="-1" role="dialog" aria-labelledby="passwordModalTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLongTitle"></h5>
                 </div>
                 <div class="modal-body">
                     <form>
                         <input type="hidden" name="company_id" id="company-id" readonly>
                         <input type="hidden" name="user_id" id="user-id" readonly>
                         <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Username:</label>
+                            <label for="recipient-name" class="col-form-label">'.$lang["PARTNER"]["COMPANIES"]["MODAL_USERNAME"].'</label>
                             <input type="text" class="form-control"  name="username" id="username" readonly>
                         </div>
                         <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Password:</label>
+                            <label for="recipient-name" class="col-form-label">'.$lang["PARTNER"]["COMPANIES"]["MODAL_PASSWORD"].'</label>
                             <input type="password" class="form-control checkPasswordUser" name="password" id="password">
                         </div>
                         <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">Repeat Password:</label>
+                            <label for="recipient-name" class="col-form-label">'.$lang["PARTNER"]["COMPANIES"]["MODAL_REPEAT_PASSWORD"].'</label>
                             <input type="password" class="form-control checkPasswordUser" id="repeat-password">
-                            <small id="PasswordRequirements">Password did not contain at least one lowercase letter, one uppercase letter, one numeric digit, and one special character</small>
+                            <small id="PasswordRequirements">'.$lang["PARTNER"]["COMPANIES"]["MODAL_PASSWORD_DESCRIPTION"].'</small>
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary closeModal">Close</button>
-                    <button type="button" class="btn btn-primary confirm" disabled>Change password</button>
+                    <button type="button" class="btn btn-secondary closeModal">'.$lang["PARTNER"]["COMPANIES"]["MODAL_CLOSE"].'</button>
+                    <button type="button" class="btn btn-primary confirm" disabled>'.$lang["PARTNER"]["COMPANIES"]["MODAL_SUBMIT_BTN"].'</button>
                 </div>
             </div>
         </div>
     </div>
-
-
-<?php
-
+';
 echo foot();
