1121c273053071d41046fd4a02e6ffe5f481b1c0
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-31 16:59:51 +0200

    Create da-dk.php

---
+++ language/da-dk.php
@@ -0,0 +1,346 @@
+<?php
+$lang["SIGN_OUT"] = "Log ud";
+$lang["LOGIN"]["HEADLINE"] = "Log på";
+$lang["LOGIN"]["EMAIL"] = "Brugernavn";
+$lang["LOGIN"]["PASSWORD"] = "Adgangskode";
+$lang["LOGIN"]["AS_PAYFAC"] = "Log på som PF/PSP";
+$lang["LOGIN"]["SIGN_IN"] = "Log på";
+$lang["LOGIN"]["ACCOUNT_UNAVAILABLE"] = "Kontoen er midlertidigt blokeret grundet for mange loginforsøg. Prøv igen om 24 timer.";
+
+
+$lang["PARTNER"]["COMPANIES"]["HEADER"] = "Virksomheder";
+$lang["PARTNER"]["COMPANIES"]["ADD_NEW"] = "Tilføj ny virksomhed";
+$lang["PARTNER"]["COMPANIES"]["INFO"] = "Info";
+$lang["PARTNER"]["COMPANIES"]["ONBOARDING"] = "Onboarding";
+$lang["PARTNER"]["COMPANIES"]["RESET_PASSWORD"] = "Nulstil adgangskode";
+$lang["PARTNER"]["COMPANIES"]["CLOSE_ACCOUNT"] = "Luk konto";
+$lang["PARTNER"]["COMPANIES"]["COMPANY_ID"] = "VirksomhedsID";
+$lang["PARTNER"]["COMPANIES"]["SECRET_KEY"] = "Payment Key";
+$lang["PARTNER"]["COMPANIES"]["COMPANY_NAME"] = "Navn på virksomhed";
+$lang["PARTNER"]["COMPANIES"]["DOMAINS"] = "Domænenavne";
+$lang["PARTNER"]["COMPANIES"]["LOGIN"] = "Login";
+$lang["PARTNER"]["COMPANIES"]["UNPAID_INVOICES"] = "Åbne regninger";
+$lang["PARTNER"]["COMPANIES"]["TOTAL_INVOICES"] = "Totale regninger";
+$lang["PARTNER"]["COMPANIES"]["STATUS"] = "Status";
+$lang["PARTNER"]["COMPANIES"]["MODAL_USERNAME"] = "Brugernavn:";
+$lang["PARTNER"]["COMPANIES"]["MODAL_PASSWORD"] = "Adgangskode:";
+$lang["PARTNER"]["COMPANIES"]["MODAL_REPEAT_PASSWORD"] = "Gentag adgangskode:";
+$lang["PARTNER"]["COMPANIES"]["MODAL_PASSWORD_DESCRIPTION"] = "Adgangskoden skal være minimum 16 karakterer samt indeholde et lille bogstav, et stort bogstav, et tal og et specialtegn.";
+$lang["PARTNER"]["COMPANIES"]["MODAL_CLOSE"] = "Luk";
+$lang["PARTNER"]["COMPANIES"]["MODAL_SUBMIT_BTN"] = "Skift adgangskode";
+
+$lang["PARTNER"]["INVOICES"]["HEADER"] = "Regninger";
+$lang["PARTNER"]["INVOICES"]["ADD_NEW"] = "Lav ny regning";
+$lang["PARTNER"]["INVOICES"]["ID"] = "ID";
+$lang["PARTNER"]["INVOICES"]["COMPANY"] = "Virksomhed";
+$lang["PARTNER"]["INVOICES"]["PACKAGE"] = "Pakke";
+$lang["PARTNER"]["INVOICES"]["AMOUNT"] = "Beløb";
+$lang["PARTNER"]["INVOICES"]["VAT"] = "Moms";
+$lang["PARTNER"]["INVOICES"]["PERIOD_END"] = "Periodens afslutning";
+$lang["PARTNER"]["INVOICES"]["PAID"] = "Betalt";
+$lang["PARTNER"]["INVOICES"]["FUNCTIONS"] = "#";
+$lang["PARTNER"]["INVOICES"]["CANCELLED"] = "Anulleret";
+$lang["PARTNER"]["INVOICES"]["PAID_TXT"] = "Betalt";
+$lang["PARTNER"]["INVOICES"]["UNPAID"] = "Ikke betalt";
+$lang["PARTNER"]["INVOICES"]["SHOW"] = "Vis";
+
+
+$lang["PARTNER"]["INVOICES_PLAN"]["HEADER"] = "Månedabonnementer";
+$lang["PARTNER"]["INVOICES_PLAN"]["ADD_NEW"] = "Opret nyt månedsskema";
+$lang["PARTNER"]["INVOICES_PLAN"]["ID"] = "ID";
+$lang["PARTNER"]["INVOICES_PLAN"]["NAME"] = "Navn";
+$lang["PARTNER"]["INVOICES_PLAN"]["CURRENCY"] = "Valuta";
+$lang["PARTNER"]["INVOICES_PLAN"]["MONTHLY_FEE"] = "Månedsabonnement";
+$lang["PARTNER"]["INVOICES_PLAN"]["TNX_FEE"] = "Transaktionsgebyr";
+$lang["PARTNER"]["INVOICES_PLAN"]["STATUS"] = "Status";
+$lang["PARTNER"]["INVOICES_PLAN"]["INFO"] = "Info";
+
+$lang["PARTNER"]["DATA"]["HEADER"] = "Partner Data";
+$lang["PARTNER"]["DATA"]["PARTNER_ID"] = "Partner ID:";
+$lang["PARTNER"]["DATA"]["KEY_1"] = "Nøgle 1:";
+$lang["PARTNER"]["DATA"]["KEY_2"] = "Nøgle 2:";
+$lang["PARTNER"]["DATA"]["DETAILS"] = "Partner detaljer";
+$lang["PARTNER"]["DATA"]["NAME"] = "Partner navn:";
+$lang["PARTNER"]["DATA"]["COUNTRY"] = "Land:";
+$lang["PARTNER"]["DATA"]["PARTNER_INVOICES"] = "Partner fakturaer";
+$lang["PARTNER"]["DATA"]["PAYMENT_SLIP"] = "Faktura notits til manuel betaling:";
+$lang["PARTNER"]["DATA"]["ACQUIRERS"] = "Indløsere";
+$lang["PARTNER"]["DATA"]["ACQUIRERS_NAME"] = "Navn";
+$lang["PARTNER"]["DATA"]["ACQUIRERS_ID"] = "ID";
+$lang["PARTNER"]["DATA"]["ACQUIRERS_WEBSITE"] = "Website";
+$lang["PARTNER"]["DATA"]["SAVE"] = "Gem";
+
+
+$lang["PARTNER"]["USERS"]["HEADER"] = "Brugere";
+$lang["PARTNER"]["USERS"]["ADD_NEW"] = "Tilføj ny bruger";
+$lang["PARTNER"]["USERS"]["ID"] = "ID";
+$lang["PARTNER"]["USERS"]["USERNAME"] = "Brugernavn";
+$lang["PARTNER"]["USERS"]["ADMINISTRATOR"] = "Administrator";
+$lang["PARTNER"]["USERS"]["FUNCTIONS"] = "#";
+$lang["PARTNER"]["USERS"]["RESET_PASSWORD"] = "Nulstil adgangskode";
+$lang["PARTNER"]["USERS"]["CLOSE_ACCOUNT"] = "Luk konto";
+$lang["PARTNER"]["USERS"]["MODAL_USERNAME"] = "Brugernavn:";
+$lang["PARTNER"]["USERS"]["MODAL_PASSWORD"] = "Adgangskode:";
+$lang["PARTNER"]["USERS"]["MODAL_REPEAT_PASSWORD"] = "Gentag adgangaskode:";
+$lang["PARTNER"]["USERS"]["MODAL_PASSWORD_DESCRIPTION"] = "Adgangskoden skal være minimum 16 karakterer samt indeholde et lille bogstav, et stort bogstav, et tal og et specialtegn.";
+$lang["PARTNER"]["USERS"]["MODAL_CLOSE"] = "Luk";
+$lang["PARTNER"]["USERS"]["MODAL_SUBMIT_BTN"] = "Skift adgangskode";
+
+$lang["PARTNER"]["USERS_ADD"]["HEADER"] = "Tilføj ny bruger";
+$lang["PARTNER"]["USERS_ADD"]["EMAIL"] = "Email:";
+$lang["PARTNER"]["USERS_ADD"]["PASSWORD"] = "Adgangskode:";
+$lang["PARTNER"]["USERS_ADD"]["CREATE_USER"] = "Opret bruger";
+
+$lang["PARTNER"]["PLUGINS"]["SETTINGS"] = "Indstillinger";
+$lang["PARTNER"]["PLUGINS"]["INSTALL"] = "Installer";
+$lang["PARTNER"]["PLUGINS"]["UNINSTALL"] = "Afinstaller";
+$lang["PARTNER"]["PLUGINS"]["CLOSE"] = "Luk";
+$lang["PARTNER"]["PLUGINS"]["SAVE"] = "Gem";
+
+
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION"]["HEADER"] = "Udgående kommunikation";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION"]["SYNC"] = "Hent en kopi af alle templates";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION"]["ADD_NEW"] = "Tilføj template";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION"]["HOOK"] = "Hook";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION"]["TYPE"] = "Kommunikationsform";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION"]["TITLE"] = "Titel";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION"]["ACTIVE"] = "Aktiv";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION"]["FUNCTION"] = "#";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION"]["EDIT"] = "Rediger";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION"]["CLOSE"] = "Fjern template";
+
+
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION_ADD"]["HEADER"] = "Template";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION_ADD"]["HOOK"] = "Hook";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION_ADD"]["TYPE"] = "Type";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION_ADD"]["TITLE"] = "Titel";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION_ADD"]["CONTENT"] = "Indhold";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION_ADD"]["ACTIVE"] = "Aktiv";
+$lang["PARTNER"]["OUTBOUND_COMMUNICATION_ADD"]["RECEIVER"] = "Modtager";
+
+$lang["PARTNER"]["MENUS"]["HEADER_PARTNER"] = "Partner Menu";
+$lang["PARTNER"]["MENUS"]["HEADER_COMPANY"] = "Virksomhedsmenu";
+$lang["PARTNER"]["MENUS"]["REMOVE"] = "(Fjern menupunkt)";
+
+$lang["PARTNER"]["DASHBOARD"]["OUTDATED_VERSION"] = "Du benytter ikke den nyeste version af Admin!";
+$lang["PARTNER"]["DASHBOARD"]["UPDATE_HERE"] = "Klik her for at opdatere";
+
+
+$lang["COMPANY"]["DASHBOARD"]["HEADER"] = "Transaktioner";
+$lang["COMPANY"]["DASHBOARD"]["PAYMENT_TYPE"] = "Transaktions type";
+$lang["COMPANY"]["DASHBOARD"]["ALL"] = "Alle";
+$lang["COMPANY"]["DASHBOARD"]["AUTH"] = "AUTH";
+$lang["COMPANY"]["DASHBOARD"]["CAPTURE"] = "Opkrævninger";
+$lang["COMPANY"]["DASHBOARD"]["REFUND"] = "Refunderinger";
+$lang["COMPANY"]["DASHBOARD"]["SECURE"] = "SECURE";
+$lang["COMPANY"]["DASHBOARD"]["CRYPT"] = "CRYPT";
+$lang["COMPANY"]["DASHBOARD"]["PAYMENT_RESULT"] = "Resultat for transaktion";
+$lang["COMPANY"]["DASHBOARD"]["ACK"] = "Godkendt";
+$lang["COMPANY"]["DASHBOARD"]["NOK"] = "Afvist";
+$lang["COMPANY"]["DASHBOARD"]["FUNCTION"] = "#";
+$lang["COMPANY"]["DASHBOARD"]["TIMESTAMP"] = "Tidsstempel";
+$lang["COMPANY"]["DASHBOARD"]["METHOD"] = "Metode";
+$lang["COMPANY"]["DASHBOARD"]["CARDHOLDER"] = "Kortholder";
+$lang["COMPANY"]["DASHBOARD"]["AMOUNT"] = "Beløb";
+$lang["COMPANY"]["DASHBOARD"]["CURRENCY"] = "Valuta";
+$lang["COMPANY"]["DASHBOARD"]["STATUS"] = "Status";
+$lang["COMPANY"]["DASHBOARD"]["INFO"] = "Info";
+
+
+$lang["COMPANY"]["CHARTS"]["HEADER"] = "Statistikker";
+
+
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["HEADER"] = "Virtuel Terminal";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["FAILED_PAYMENT"] = "Betalingen fejlede, forsøg venligst igen.";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["CARDHOLDER"] = "Kortholder";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["CARDNO"] = "Kortnummer";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["EXPMONTH"] = "Udløbsmåned";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["EXPYEAR"] = "Udløbsår";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["CVV"] = "CVV";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["CONFIRMED"] = "Knap";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["CONFIRMED_BTN"] = "Gennemfør";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["CURRENCY"] = "Valuta:";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["AMOUNT"] = "Beløb:";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["ORDER_ID"] = "Order ID:";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["STORE_CARD"] = "Gem kortet til senere opkrævninger:";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["TYPE"] = "Type:";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["MOTO"] = "MOTO";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["ECOM"] = "ECOM";
+$lang["COMPANY"]["VIRTUAL_TERMINAL"]["START_TERMINAL"] = "Start virtuel terminal";
+
+$lang["COMPANY"]["DATA"]["HEADER"] = "Virksomhedsoplysninger";
+$lang["COMPANY"]["DATA"]["MERCHANT_ID"] = "Virksomheds ID:";
+$lang["COMPANY"]["DATA"]["API_PASSWORD"] = "API Kodeord:";
+$lang["COMPANY"]["DATA"]["PAYMENT_PASSWORD"] = "Payment Kodeord:";
+$lang["COMPANY"]["DATA"]["HEADER_SESSION"] = "Session Data";
+$lang["COMPANY"]["DATA"]["USER_ID"] = "ID:";
+$lang["COMPANY"]["DATA"]["SESSION_KEY"] = "Sessions nøgle:";
+$lang["COMPANY"]["DATA"]["HEADER_COMPANY"] = "Virksomhedsdetaljer";
+$lang["COMPANY"]["DATA"]["COMPANY_NAME"] = "Navn:";
+$lang["COMPANY"]["DATA"]["REGNO"] = "Registeringsnummer/CVR-nummer:";
+$lang["COMPANY"]["DATA"]["EUVAT"] = "CVR-nummer med DK foran:";
+$lang["COMPANY"]["DATA"]["ADDRESS"] = "Adresse:";
+$lang["COMPANY"]["DATA"]["POSTAL"] = "Postnummer:";
+$lang["COMPANY"]["DATA"]["CITY"] = "By:";
+$lang["COMPANY"]["DATA"]["COUNTRY"] = "Land:";
+$lang["COMPANY"]["DATA"]["PHONE"] = "Telefonnummer:";
+$lang["COMPANY"]["DATA"]["CARDHOLDER_DESCRIPTION"] = "Tekst på kundens kontoudtog:";
+$lang["COMPANY"]["DATA"]["ACQUIRERS"] = "Indløsere";
+$lang["COMPANY"]["DATA"]["NAME"] = "Navn";
+$lang["COMPANY"]["DATA"]["ID"] = "ID";
+$lang["COMPANY"]["DATA"]["WEBSITE"] = "Website";
+$lang["COMPANY"]["DATA"]["ACQUIRER_RULES"] = "Indløser regler";
+$lang["COMPANY"]["DATA"]["ROUND_ROBIN"] = "Round Robin";
+$lang["COMPANY"]["DATA"]["ROUND_ROBIN_WITH_MAX"] = "Round Robin med maks beløb";
+$lang["COMPANY"]["DATA"]["ACQUIRER_WITH_LOWEST_VOLUME"] = "Acquirer med lavest omsætning får næste transaktion";
+$lang["COMPANY"]["DATA"]["ACQUIRER_WITH_LOWEST_VOLUME_MAX"] = "Acquirer med lavest omsætning, men med maks beløb";
+$lang["COMPANY"]["DATA"]["RULE_SETTINGS"] = "Reglindstillinger";
+$lang["COMPANY"]["DATA"]["DAILY_MAX"] = "Dagsmaks";
+$lang["COMPANY"]["DATA"]["SUPPORTED_BRANDS"] = "Brands";
+$lang["COMPANY"]["DATA"]["DATA"] = "Data";
+$lang["COMPANY"]["DATA"]["SETTINGS"] = "Indstillinger";
+$lang["COMPANY"]["DATA"]["SAVE"] = "Gem";
+$lang["COMPANY"]["DATA"]["CLOSE"] = "Luk";
+
+$lang["COMPANY"]["PAYOUTS"]["HEADER"] = "Udbetalinger";
+$lang["COMPANY"]["PAYOUTS"]["DATE"] = "Dato";
+$lang["COMPANY"]["PAYOUTS"]["GROSS_VOLUME"] = "Total omsætning";
+$lang["COMPANY"]["PAYOUTS"]["CALCULATED_FEE"] = "Beregnet gebyr";
+$lang["COMPANY"]["PAYOUTS"]["SETTLEMENT"] = "Til udbetaling";
+$lang["COMPANY"]["PAYOUTS"]["SETTLED"] = "Udbetalt";
+$lang["COMPANY"]["PAYOUTS"]["INFO"] = "Info";
+
+$lang["COMPANY"]["ONBOARDING"]["HEADER"] = "Merchant Onboarding";
+
+
+$lang["COMPANY"]["USERS"]["HEADER"] = "Brugere";
+$lang["COMPANY"]["USERS"]["ADD_NEW"] = "Tilføj ny bruger";
+$lang["COMPANY"]["USERS"]["ID"] = "ID";
+$lang["COMPANY"]["USERS"]["USER_NAME"] = "Brugernavn";
+$lang["COMPANY"]["USERS"]["ADMINISTRATOR"] = "Administrator";
+$lang["COMPANY"]["USERS"]["FUNCTION"] = "#";
+$lang["COMPANY"]["USERS"]["RESET_PASSWORD"] = "Nulstil adgangskode";
+$lang["COMPANY"]["USERS"]["CLOSE_ACCOUNT"] = "Luk konto";
+$lang["COMPANY"]["USERS"]["PASSWORD"] = "Adgangskode";
+$lang["COMPANY"]["USERS"]["REPEAT_PASSWORD"] = "Gentag adgangskode";
+$lang["COMPANY"]["USERS"]["PASSWORD_REQUIREMENTS"] = "Adgangskoden skal være minimum 16 karakterer samt indeholde et lille bogstav, et stort bogstav, et tal og et specialtegn.";
+$lang["COMPANY"]["USERS"]["CLOSE"] = "Luk konto";
+$lang["COMPANY"]["USERS"]["CHANGE_PASSWORD"] = "Skift adgangskode";
+
+$lang["COMPANY"]["USERS_ADD"]["HEADER"] = "Tilføj ny bruger";
+$lang["COMPANY"]["USERS_ADD"]["NAME"] = "Navn:";
+$lang["COMPANY"]["USERS_ADD"]["EMAIL"] = "Email:";
+$lang["COMPANY"]["USERS_ADD"]["PHONENO"] = "Telefonnummer:";
+$lang["COMPANY"]["USERS_ADD"]["PASSWORD"] = "Adgangskode:";
+$lang["COMPANY"]["USERS_ADD"]["CREATE_USER"] = "Opret konto";
+
+
+$lang["COMPANY"]["DISPUTES"]["HEADER"] = "Indsigelser";
+$lang["COMPANY"]["DISPUTES"]["TYPE"] = "Indsigelses type:";
+$lang["COMPANY"]["DISPUTES"]["OPEN"] = "Åben";
+$lang["COMPANY"]["DISPUTES"]["CLOSED"] = "Lukket";
+$lang["COMPANY"]["DISPUTES"]["STATUS"] = "Status";
+$lang["COMPANY"]["DISPUTES"]["RECEIVED"] = "Modtaget";
+$lang["COMPANY"]["DISPUTES"]["REPRESENTED"] = "Repræsenteret";
+$lang["COMPANY"]["DISPUTES"]["FIRST"] = "1. indsigelse";
+$lang["COMPANY"]["DISPUTES"]["SECOND"] = "2. indsigelse";
+$lang["COMPANY"]["DISPUTES"]["LOST"] = "Tabt";
+$lang["COMPANY"]["DISPUTES"]["CHANGE_VIEW"] = "Skift visning";
+$lang["COMPANY"]["DISPUTES"]["ID"] = "Indsigelses ID";
+$lang["COMPANY"]["DISPUTES"]["TRANSACTION_ID"] = "Transaktions ID";
+$lang["COMPANY"]["DISPUTES"]["DISPUTED_AMOUNT"] = "Indsigelsesbeløb";
+$lang["COMPANY"]["DISPUTES"]["ORDER_AMOUNT"] = "Ordre beløb";
+$lang["COMPANY"]["DISPUTES"]["CURRENCY"] = "Valuta";
+$lang["COMPANY"]["DISPUTES"]["STATUS"] = "Status";
+$lang["COMPANY"]["DISPUTES"]["INFO"] = "Info";
+
+$lang["COMPANY"]["PAYMENT_LINKS"]["HEADER"] = "Betalingslinks";
+$lang["COMPANY"]["PAYMENT_LINKS"]["LINK_SENT"] = "Dit betalingslink er blevet sendt";
+$lang["COMPANY"]["PAYMENT_LINKS"]["PLACEHOLDER_SENDER"] = "Afsender email";
+$lang["COMPANY"]["PAYMENT_LINKS"]["SENDER"] = "Afsender";
+$lang["COMPANY"]["PAYMENT_LINKS"]["PLACEHOLDER_RECIPIENT"] = "Modtager email";
+$lang["COMPANY"]["PAYMENT_LINKS"]["RECIPIENT"] = "Modtager";
+$lang["COMPANY"]["PAYMENT_LINKS"]["EXPIRY"] = "Udløbstidspunkt for betalingslink";
+$lang["COMPANY"]["PAYMENT_LINKS"]["PLACEHOLDER_ORDER_ID"] = "Ordre ID for betalingslink";
+$lang["COMPANY"]["PAYMENT_LINKS"]["ORDER_ID"] = "Ordre ID";
+$lang["COMPANY"]["PAYMENT_LINKS"]["PLACEHOLDER_AMOUNT_PAID"] = "Beløb som skal betales eksempelvis 1,00";
+$lang["COMPANY"]["PAYMENT_LINKS"]["AMOUNT_PAID"] = "Beløb til betaling";
+$lang["COMPANY"]["PAYMENT_LINKS"]["CURRENCY"] = "Valuta";
+$lang["COMPANY"]["PAYMENT_LINKS"]["SUBMIT"] = "Send betalingslink";
+
+
+$lang["COMPANY"]["INVOICES"]["HEADER"] = "Udstedte fakturaer";
+$lang["COMPANY"]["INVOICES"]["ID"] = "ID";
+$lang["COMPANY"]["INVOICES"]["AMOUNT"] = "Beløb";
+$lang["COMPANY"]["INVOICES"]["VAT"] = "Moms";
+$lang["COMPANY"]["INVOICES"]["PERIOD_END"] = "Periode afslutning";
+$lang["COMPANY"]["INVOICES"]["PAID"] = "Betalt";
+$lang["COMPANY"]["INVOICES"]["FUNCTION"] = "#";
+$lang["COMPANY"]["INVOICES"]["SHOW"] = "Vis";
+$lang["COMPANY"]["INVOICES"]["CANCELLED"] = "Anulleret";
+$lang["COMPANY"]["INVOICES"]["PAID_TEXT"] = "Betalt";
+$lang["COMPANY"]["INVOICES"]["UNPAID"] = "Ubetalt";
+
+$lang["COMPANY"]["INVOICES_SHOW"]["HEADER"] = "Faktura";
+$lang["COMPANY"]["INVOICES_SHOW"]["DATES"] = "Datoer";
+$lang["COMPANY"]["INVOICES_SHOW"]["ISSUING_DATE"] = "Udstedelsesdato";
+$lang["COMPANY"]["INVOICES_SHOW"]["PERIOD_START"] = "Startdato";
+$lang["COMPANY"]["INVOICES_SHOW"]["PERIOD_END"] = "Slut dato";
+$lang["COMPANY"]["INVOICES_SHOW"]["PURCHASED_PRODUCTS"] = "Købte produkter";
+$lang["COMPANY"]["INVOICES_SHOW"]["BILLING"] = "Ordre oplysninger";
+$lang["COMPANY"]["INVOICES_SHOW"]["COMPANY_NAME"] = "Virksomhedsnavn";
+$lang["COMPANY"]["INVOICES_SHOW"]["VAT"] = "Moms";
+$lang["COMPANY"]["INVOICES_SHOW"]["ADDRESS"] = "Addresse";
+$lang["COMPANY"]["INVOICES_SHOW"]["POSTAL"] = "Postnummer";
+$lang["COMPANY"]["INVOICES_SHOW"]["CITY"] = "By";
+$lang["COMPANY"]["INVOICES_SHOW"]["COUNTRY"] = "Land";
+
+$lang["COMPANY"]["CARDHOLDER_SUBSCRIPTIONS"]["HEADER"] = "Abonnementer";
+$lang["COMPANY"]["CARDHOLDER_SUBSCRIPTIONS"]["RESULT"] = "Abonnement resultat:";
+$lang["COMPANY"]["CARDHOLDER_SUBSCRIPTIONS"]["ALL"] = "Alle";
+$lang["COMPANY"]["CARDHOLDER_SUBSCRIPTIONS"]["ACK"] = "Godkendte";
+$lang["COMPANY"]["CARDHOLDER_SUBSCRIPTIONS"]["NOK"] = "Afviste";
+$lang["COMPANY"]["CARDHOLDER_SUBSCRIPTIONS"]["TRANSACTION_ID"] = "Transaktions beløb";
+$lang["COMPANY"]["CARDHOLDER_SUBSCRIPTIONS"]["CARDHOLDER"] = "Kortholder";
+$lang["COMPANY"]["CARDHOLDER_SUBSCRIPTIONS"]["CARD_HASHED"] = "Kortnummer";
+$lang["COMPANY"]["CARDHOLDER_SUBSCRIPTIONS"]["EXPIRY"] = "Udløbstidspunkt";
+$lang["COMPANY"]["CARDHOLDER_SUBSCRIPTIONS"]["STATUS"] = "Status";
+
+
+
+$lang["COMPANY"]["PAYMENT"]["HEADER"] = "Betalingsdata";
+$lang["COMPANY"]["PAYMENT"]["STATUS"] = "Status:";
+$lang["COMPANY"]["PAYMENT"]["TIMESTAMP"] = "Tidsstempel:";
+$lang["COMPANY"]["PAYMENT"]["ACTION"] = "Handling:";
+$lang["COMPANY"]["PAYMENT"]["AMOUNT"] = "Beløb:";
+$lang["COMPANY"]["PAYMENT"]["CURRENCY"] = "Valuta:";
+$lang["COMPANY"]["PAYMENT"]["ACQUIRER_RESPONSE"] = "Indløser svar";
+$lang["COMPANY"]["PAYMENT"]["RELATED_PAYMENTS"] = "Relaterede betalinger";
+$lang["COMPANY"]["PAYMENT"]["FUNCTION"] = "#";
+$lang["COMPANY"]["PAYMENT"]["METHOD"] = "Metode";
+$lang["COMPANY"]["PAYMENT"]["CARDHOLDER"] = "Kortholder";
+$lang["COMPANY"]["PAYMENT"]["AMOUNT"] = "Beløb";
+$lang["COMPANY"]["PAYMENT"]["CURRENCY"] = "Valuta";
+$lang["COMPANY"]["PAYMENT"]["STATUS"] = "Status";
+$lang["COMPANY"]["PAYMENT"]["INFO"] = "Info";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
