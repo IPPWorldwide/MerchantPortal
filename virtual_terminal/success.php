8e832c90c04ee312413eeb11f2c069f06d865688
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-31 18:25:23 +0200

    Create success.php

---
+++ virtual_terminal/success.php
@@ -0,0 +1,7 @@
+<?php
+include("../base.php");
+$merchant_data = $ipp->MerchantData();
+echo head();
+
+echo "<div style='height: 100%;width:100%;'><center><h2>Confirmed Payment</h2><br /><img src='/theme/".$_ENV["THEME"]."/assets/img/128/check_sign_icon.png'></center></div>";
+echo foot();
