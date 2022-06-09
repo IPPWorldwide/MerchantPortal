8ac07707af21fae1651988a29877d54d06c1b6c7
Mathias Gajhede <mag@venturecapitals.dk>
2022-06-09 02:33:25 +0200

    Display error

---
+++ CHANGES.md
@@ -0,0 +1,1 @@
+- Minor bugfix on display error at Partner Data
--- partner/data/index.php
+++ partner/data/index.php
@@ -34,7 +34,7 @@ echo '
             </div>
             <h2>'.$lang["PARTNER"]["DATA"]["PARTNER_INVOICES"].'</h2>
             <div class="row row-cols-md-12 mb-12">
-                <div class="col themed-grid-col"'.$lang["PARTNER"]["DATA"]["PAYMENT_SLIP"].'<br /><input name="meta[invoicetext]" class="form-control" value="'; echo $partner_data->meta_data->meta->invoicetext ?: ""; echo '">
+                <div class="col themed-grid-col"'.$lang["PARTNER"]["DATA"]["PAYMENT_SLIP"].'<br /><input name="meta[invoicetext]" class="form-control" value="'; echo $partner_data->meta_data->meta->invoicetext ?? ""; echo '">
             </div>
             <div class="row row-cols-md-2 mb-2">
                 <div class="col themed-grid-col">
