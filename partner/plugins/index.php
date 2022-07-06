380fee969d246e166df624aec230552e29c7e586
Mathias Gajhede <mag@venturecapitals.dk>
2022-07-06 17:55:25 +0200

    Merge issue

--- partner/plugins/index.php
+++ partner/plugins/index.php
@@ -21,35 +21,72 @@ echo head();
 
 echo '<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 ';
+$i=1;
 foreach($all_plugins as $key=>$value) {
     // removing plugnings that are stored in remote api.
     unset($all_available_plugins[$key]);
+
     echo '
         <div class="col" data-plugin-id="'.$key.'">
           <div class="card shadow-sm">
               <p class="card-header">'.$value->name.'</p>
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-            <div class="card-body">
-              <p class="card-text">'.$value->description->en_gb->description.'</p>
+            <div class="card-body plugin-card-body">
+              <p class="card-text">'.substr($value->description->en_gb->description, 0, 199); if(strlen($value->description->en_gb->description) > 199){ echo '...'; } echo '</p>
               <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-info pluginSettingsModal" data-plugin-name="'.$key.'" data-plugin-title="'.$value->name.'" data-fields=\''.$plugins->getSettingsFields($key).'\' data-values=\''.$plugins->getSettingsValues($key,"").'\'>'.$lang["PARTNER"]["PLUGINS"]["SETTINGS"].'</button>';
+                <div class="btn-group">';
     if(!file_exists(BASEDIR . "plugins/".$key))
-        echo '<button type="button" data-plugin-name="'.$key.'" data-plugin-file="'.$value->file.'" class="btn btn-sm btn-success installModal">'.$lang["PARTNER"]["PLUGINS"]["INSTALL"].'</button>';
+        echo '<button type="button" data-plugin-name="'.$key.'" data-plugin-key="'.$i.'" data-plugin-file="'.$value->file.'" class="btn btn-sm btn-success installModal me-2 plugin-btn-'.$i.'">'.$lang["PARTNER"]["PLUGINS"]["INSTALL"].'</button>';
     else
-        echo '<button type="button" data-local-plugin="0" data-plugin-id="'.$plugins->getSettingsValues($key,"plugin_id").'" data-plugin-name="'.$key.'" class="btn btn-sm btn-danger removeModal">'.$lang["PARTNER"]["PLUGINS"]["UNINSTALL"].'</button>';
+        echo '<button type="button" data-plugin-id="'.$plugins->getSettingsValues($key,"plugin_id").'" data-plugin-key="'.$i.'" data-plugin-name="'.$key.'" class="btn btn-sm btn-danger me-2 removeModal plugin-btn-'.$i.'">'.$lang["PARTNER"]["PLUGINS"]["UNINSTALL"].'</button>';
+    ;
+    echo
+        "<button type='button' class='btn btn-sm btn-info text-white' data-bs-toggle='modal' data-bs-target='#pluginViewMoreModal".$i."'>".$lang["PARTNER"]["PLUGINS"]["VIEW_MORE"]."</button>";
     echo '
                 </div>
               </div>
             </div>
           </div>
-        </div>';
+        </div>
+        <div class="modal fade" id="pluginViewMoreModal'.$i.'" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                '.$value->name.'
+                <h5 class="modal-title" id="exampleModalLongTitle"></h5>
+                </div>
+                <div class="modal-body" id="pluginViewMoreContent">
+                <div class="col">
+                <div class="card shadow-sm">
+                    
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                    <div class="card-body">
+                    <p class="card-text">'.$value->description->en_gb->description.'</p>
+                    </div>
+                </div>
+                </div>    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">'.$lang["PARTNER"]["PLUGINS"]["CLOSE"].'</button>';
+    $plugins->getSettingsValues($key,"").'\'>'.$lang["PARTNER"]["PLUGINS"]["SETTINGS"].'</button>';
+    if(!file_exists(BASEDIR . "plugins/".$key))
+        echo '<button type="button" data-plugin-name="'.$key.'" data-plugin-file="'.$value->file.'" class="btn btn-sm btn-success installModal plugin-btn-'.$i.'">'.$lang["PARTNER"]["PLUGINS"]["INSTALL"].'</button>';
+    else
+        echo '<button type="button" class="btn btn-sm btn-info text-white pluginSettingsModal me-2" data-plugin-name="'.$key.'" data-plugin-title="'.$value->name.'" data-bs-dismiss="modal" data-fields=\''.$plugins->getSettingsFields($key).'\' data-values=\''.$plugins->getSettingsValues($key,"").'\'>'.$lang["PARTNER"]["PLUGINS"]["SETTINGS"].'</button>
+
+                        <button type="button" data-plugin-id="'.$plugins->getSettingsValues($key,"plugin_id").'" data-plugin-key="'.$i.'" data-plugin-name="'.$key.'" class="btn btn-sm btn-danger removeModal plugin-btn-'.$i.'">'.$lang["PARTNER"]["PLUGINS"]["UNINSTALL"].'</button>';
+    echo '
+                </div>
+            </div>
+        </div>
+    </div>';
+    $i++;
 }
 // Check if any plugins are locally installed!
 if(!is_null($all_available_plugins)){
-  // Loop throug all locally installed plugins that are not in api call!
-  foreach($all_available_plugins as $key=>$value) {
-      echo '
+    // Loop throug all locally installed plugins that are not in api call!
+    foreach($all_available_plugins as $key=>$value) {
+        echo '
           <div class="col" data-plugin-id="'.$key.'">
             <div class="card shadow-sm">
                 <p class="card-header">'.$value->id.'</p>
@@ -59,20 +96,18 @@ if(!is_null($all_available_plugins)){
                 <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
                     <button type="button" class="btn btn-sm btn-info pluginSettingsModal" data-plugin-name="'.$key.'" data-plugin-title="'.$value->id.'" data-fields=\''.$plugins->getSettingsFields($key).'\' data-values=\''.$plugins->getSettingsValues($key,"").'\'>'.$lang["PARTNER"]["PLUGINS"]["SETTINGS"].'</button>';
-      if(!file_exists(BASEDIR . "plugins/".$key))
-          echo '<button type="button" data-plugin-name="'.$key.'" data-plugin-file="" class="btn btn-sm btn-success installModal">'.$lang["PARTNER"]["PLUGINS"]["INSTALL"].'</button>';
-      else
-          echo '<button type="button" data-local-plugin="1" data-plugin-id="'.$plugins->getSettingsValues($key,"plugin_id").'" data-plugin-name="'.$key.'" class="btn btn-sm btn-danger removeModal">'.$lang["PARTNER"]["PLUGINS"]["UNINSTALL"].'</button>';
-      echo '
+        if(!file_exists(BASEDIR . "plugins/".$key))
+            echo '<button type="button" data-plugin-name="'.$key.'" data-plugin-file="" class="btn btn-sm btn-success installModal">'.$lang["PARTNER"]["PLUGINS"]["INSTALL"].'</button>';
+        else
+            echo '<button type="button" data-local-plugin="1" data-plugin-id="'.$plugins->getSettingsValues($key,"plugin_id").'" data-plugin-name="'.$key.'" class="btn btn-sm btn-danger removeModal">'.$lang["PARTNER"]["PLUGINS"]["UNINSTALL"].'</button>';
+        echo '
                   </div>
                   <div>Only local</div>
                 </div>
               </div>
-            </div>
-          </div>';
-  }
+            </div>';
 }
-echo "</div>";
+}
 echo '
     <div class="modal fade" id="pluginModal" tabindex="-1" role="dialog" aria-labelledby="pluginModalTitle" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
@@ -85,11 +120,11 @@ echo '
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary closeModal">'.$lang["PARTNER"]["PLUGINS"]["CLOSE"].'</button>
+                    <button type="button" class="btn btn-secondary closeModal" data-bs-dismiss="modal">'.$lang["PARTNER"]["PLUGINS"]["CLOSE"].'</button>
                     <button type="button" class="btn btn-primary confirmPluginSettngs">'.$lang["PARTNER"]["PLUGINS"]["SAVE"].'</button>
                 </div>
             </div>
         </div>
-    </div>
-</div>';
+    </div>';
+echo "</div>";
 echo foot();
