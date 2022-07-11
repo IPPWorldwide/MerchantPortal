1d634f43f16a443362d756ace9df58be2c6d3c60
Sumit techmsm <sumit.chauhan183@gmail.com>
2022-07-11 21:55:18 +0530

    Bug in Displaying the update of Merchant Portal

--- partner/index.php
+++ partner/index.php
@@ -1,24 +1,25 @@
 <?php
 include("b.php");
-if(isset($REQ["update"]) && $REQ["update"] == "true") {
+if(isset($REQ["update"]) && $REQ["update"] == "true"):
     header( "Location: /update.php?version=".$ipp->version()->content->version);
     die();
-}
+endif;
 echo head();
 
-if($_ENV["VERSION"] < $ipp->version()->content->version) {
-    echo "<div class=\"alert alert-warning\" role=\"alert\">".$lang["PARTNER"]["DASHBOARD"]["OUTDATED_VERSION"]."<a href='?update=true'>".$lang["PARTNER"]["DASHBOARD"]["UPDATE_HERE"]."</a></div>";
-    foreach($ipp->ListVersions() as $key=>$value) {
-        if($_ENV["VERSION"] < $key) {
-            echo "<div class=\"alert alert-warning\" role=\"alert\"><h3>$key - $value</h3><p>";
-            $pageDocument = @file_get_contents("https://raw.githubusercontent.com/IPPWorldwide/MerchantPortal/".$key."/CHANGES.md");
-            if ($pageDocument !== false) {
-                echo nl2br($pageDocument);
-            }
-            echo "</p></div>";
-        }
-    }
-}
+if($_ENV["VERSION"] < $ipp->version()->content->version):
+    ?>
+    <div class="alert alert-warning" role="alert"><?=$lang["PARTNER"]["DASHBOARD"]["OUTDATED_VERSION"]?><a href='?update=true'><?=$lang["PARTNER"]["DASHBOARD"]["UPDATE_HERE"]?></a></div>
+    <?php foreach($ipp->ListVersions() as $key=>$value):
+    if($_ENV["VERSION"] < $key):
+        echo "<div class='alert alert-warning' role='alert'><h3>$key - $value</h3><p>";
+        $pageDocument = @file_get_contents("https://raw.githubusercontent.com/IPPWorldwide/MerchantPortal/".$key."/CHANGES.md");
+        if ($pageDocument !== false):
+            echo nl2br($pageDocument);
+        endif;
+        echo "</p></div>";
+    endif;
+endforeach;
+endif;
 
 echo foot();
 
