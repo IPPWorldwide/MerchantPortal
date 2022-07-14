b3442fce8755766cb80a880f075c784f61e6801b
Mathias Gajhede <mag@venturecapitals.dk>
2022-07-14 15:30:48 +0200

    Update index.php

--- partner/plugins/index.php
+++ partner/plugins/index.php
@@ -46,7 +46,7 @@ $i=1;
             <p class="card-header">Add new plugin</p>
             <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
             <div class="card-body plugin-card-body">
-                <p class="card-text">Use your own SMTP server to send out emails and pay-by-links, instead of using our shared SMTP-service.</p>
+                <p class="card-text"></p>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group"><form id="add-new-plugin" action="#" method="POST" enctype='multipart/form-data'><input type="file" name="new-plugin" id="plugin-file" accept=".zip"></form><button type="button" class="btn btn-sm btn-info text-white" id="upload-plugin-file">Upload</button>
                     </div>
