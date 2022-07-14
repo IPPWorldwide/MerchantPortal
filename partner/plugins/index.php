5b281c7558267a9107e70fb16ec1b38ec8381b73
Mathias Gajhede <mag@venturecapitals.dk>
2022-07-14 17:07:12 +0200

    Update index.php

--- partner/plugins/index.php
+++ partner/plugins/index.php
@@ -40,6 +40,66 @@ echo head();
 
 echo '<div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 ';
+?>
+    <div class="col">
+        <div class="card shadow-sm">
+            <p class="card-header">Add new plugin</p>
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+            <div class="card-body plugin-card-body">
+                <p class="card-text"></p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group"><form id="add-new-plugin" action="#" method="POST" enctype='multipart/form-data'><input type="file" name="new-plugin" id="plugin-file" accept=".zip"></form><button type="button" class="btn btn-sm btn-info text-white" id="upload-plugin-file">Upload</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script>
+        $(document).ready(function (){
+            $('#upload-plugin-file').click(function (){
+                var vidFileLength = $("#plugin-file")[0].files.length;
+                if(vidFileLength === 0){
+                    swal({
+                        title: "Error!",
+                        text: "Please choose a file!",
+                        icon: "error",
+                    });
+                }else{
+                    var fd = new FormData();
+                    var files = $('#plugin-file')[0].files;
+                    fd.append('new-plugin',files[0]);
+                    $.ajax({
+                        url: 'plugin-validation.php',
+                        type: 'post',
+                        data: fd,
+                        contentType: false,
+                        processData: false,
+                        success: function(response){
+                            var d = JSON.parse(response);
+                            if(d.error){
+                                swal({
+                                    title: "Error!",
+                                    text: d.message,
+                                    icon: "error",
+                                });
+                            }else{
+                                swal({
+                                    title: "Success!",
+                                    text: d.message,
+                                    icon: "success",
+                                }).then(function (){
+                                    window.location.reload();
+                                });
+                            }
+                        },
+                    });
+                }
+            });
+        });
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<?php
 $i=1;
 foreach($all_plugins as $key=>$value) {
     // removing plugnings that are stored in remote api.
