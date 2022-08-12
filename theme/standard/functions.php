784f8768fc45b8cee6220eb14b74c98b8d642646
Mathias Gajhede <mag@venturecapitals.dk>
2022-08-12 13:03:41 +0200

    Update functions.php

--- theme/standard/functions.php
+++ theme/standard/functions.php
@@ -7,7 +7,7 @@ function standard_theme_menu($section, $base = "") {
         if($key == "virtual_terminal" && (isset($IPP_CONFIG["PORTAL_DEACTIVATE_VIRTUAL_TERMINAL"]) && $IPP_CONFIG["PORTAL_DEACTIVATE_VIRTUAL_TERMINAL"]))
             continue;
         $html .= "<li class=\"nav-item\">
-                <a class=\"nav-link active\" aria-current=\"page\" href=\"/".$base.$key."\">
+                <a class=\"nav-link active\" aria-current=\"page\" id='menu_".str_replace("/","_",$base.$key)."' href=\"/".$base.$key."\">
                     <span data-feather=\"home\"></span>
                     ".$value."
                 </a>
