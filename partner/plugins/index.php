02876c014456f3c43e574766aece48eb64c92997
Mathias Gajhede <mag@venturecapitals.dk>
2022-09-26 11:08:36 +0200

    Config Pages for Plugins

--- partner/plugins/index.php
+++ partner/plugins/index.php
@@ -81,7 +81,7 @@ foreach($all_plugins as $key=>$value) {
           <div class="card shadow-sm">
               <p class="card-header">'.$name.'</p>
             ';
-    if($value->image !== NULL) {
+    if(isset($value->image) && $value->image !== NULL) {
         echo '<div class="plugin-thumbnail"><img src="'.$value->image.'"></div>';
     } else {
         echo '<svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>';
