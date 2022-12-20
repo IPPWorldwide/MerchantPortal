241f922e7151794cbea70d8386fa81f80ebecef8
Mathias Gajhede <mag@venturecapitals.dk>
2022-12-20 11:28:12 +0000

    Update woocommerce.php

--- setup/shop_extensions/woocommerce.php
+++ setup/shop_extensions/woocommerce.php
@@ -20,11 +20,11 @@ sleep(1);
 rename(BASEDIR . "tmp/new_woocommerce/ippgateway.php",BASEDIR . "tmp/new_woocommerce/new_woocommerce.php");
 
 $str = file_get_contents(BASEDIR . "tmp/new_woocommerce/new_woocommerce.php");
-$str = str_replace("WC_Gateway_IPPGateway","WC_Gateway_".str_replace([" ",".","_",","],"",strtolower($_POST["portal_title"])), $str);
-$str = str_replace("IPPGATEWAY_DIR",str_replace([" ",".","_",","],"",strtolower($_POST["portal_title"]))."_DIR", $str);
+$str = str_replace("WC_Gateway_IPPGateway","WC_Gateway_NewWooCommerce", $str);
+$str = str_replace("IPPGATEWAY_DIR","NewWooCommerce_DIR", $str);
 $str = str_replace("IPPGateway Services",$_POST["portal_title"], $str);
-$str = str_replace("ippgateway",str_replace([" ",".","_",","],"",strtolower($_POST["portal_title"]))."_gateway", $str);
-$str = str_replace("ipp_hourly",str_replace([" ",".","_",","],"",strtolower($_POST["portal_title"]))."_hourly", $str);
+$str = str_replace("ippgateway","newwoocommerce_gateway", $str);
+$str = str_replace("ipp_hourly","newwoocommerce_hourly", $str);
 $str = str_replace("IPPGateway Payment",$_POST["portal_title"], $str);
 file_put_contents(BASEDIR . "tmp/new_woocommerce/new_woocommerce.php", $str);
 Zip(BASEDIR . 'tmp/new_woocommerce/', BASEDIR . 'tmp/new_woocommerce.zip');
