34b2311dda2d1ec056fb2359766c18fa6fa6d723
Mathias Gajhede <mag@venturecapitals.dk>
2022-12-21 18:29:01 +0100

    Added Pyro & Focus Fields

--- setup/js/main.js
+++ setup/js/main.js
@@ -28,6 +28,7 @@
         onStepChanging: function(event, currentIndex, newIndex) {
             if (currentIndex === 0) {
                 form.parent().parent().parent().append('<div class="footer footer-' + currentIndex + '"></div>');
+                $("#partner_id").focus();
             }
             if (currentIndex === 1) {
                 form.parent().parent().parent().find('.footer').removeClass('footer-0').addClass('footer-' + currentIndex + '');
@@ -44,6 +45,14 @@
             form.validate().settings.ignore = ":disabled,:hidden";
             return form.valid();
         },
+        onStepChanged: function(event, currentIndex) {
+            if (currentIndex === 0) {
+                $("#portal_title").focus();
+            }
+            if (currentIndex === 1) {
+                $("#partner_id").focus();
+            }
+        },
         onFinishing: function(event, currentIndex) {
             form.validate().settings.ignore = ":disabled";
             return form.valid();
@@ -59,13 +68,19 @@
             })
             .done(function( msg ) {
                 if(msg.content.security.key2 === $( "#partner_key2" ).val()) {
+                    $(".pyro").fadeIn();
+                    wordflick();
                     $.ajax({
                         method: "POST",
                         url: "?",
                         data: $("#signup-form").serialize()
                     })
                     .done(function( msg ) {
-                        window.location.replace($("#portal_url").val() + "?setup=complete");
+                        setTimeout(
+                        function()
+                        {
+                            window.location.replace($("#portal_url").val() + "?setup=complete");
+                        }, 8000);
                     });
                 } else {
                     alert( "Key 2 is invalid" );
@@ -74,13 +89,52 @@
             .fail(function() {
                 alert( "Partner ID or Key 1 is invalid" );
             });
-        },
-        onStepChanged: function(event, currentIndex, priorIndex) {
-
-            return true;
         }
     });
+    var
+    words = ['Setting up the portal ...',' ... Get ready','The new Experience ...',' ... Just a few more seconds','... Your personalized universe'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 50,
+    speed = 50;
 
+    var wordflick = function(){
+        setInterval(function(){
+            if (forwards) {
+                if(offset >= words[i].length){
+                    ++skip_count;
+                    if (skip_count == skip_delay) {
+                        forwards = false;
+                        skip_count = 0;
+                    }
+                }
+            }
+            else {
+                if(offset == 0){
+                    forwards = true;
+                    i++;
+                    offset = 0;
+                    if(i >= len){
+                        i=0;
+                    }
+                }
+            }
+            part = words[i].substr(0, offset);
+            if (skip_count == 0) {
+                if (forwards) {
+                    offset++;
+                }
+                else {
+                    offset--;
+                }
+            }
+            $('.pyro .title').text(part);
+        },speed);
+    };
     jQuery.extend(jQuery.validator.messages, {
         required: "",
         remote: "",
