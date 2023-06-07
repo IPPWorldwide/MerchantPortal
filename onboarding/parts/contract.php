cfacdd820b25ec3021131639f4563a91c10a2e81
Mathias Gajhede <mag@venturecapitals.dk>
2023-06-07 21:44:11 +0200

    Contract update

--- onboarding/parts/contract.php
+++ onboarding/parts/contract.php
@@ -3,21 +3,24 @@
 ?>
 <div id='contract'>
     <div class="step1 row our_contracts">
-        <h2>The Contract</h2>
-        Thanks you for providing all the documentation in the steps before. You are now ready to sign the financial contract with us.<br />
-        The contract is in two parts:
-        <ol>
-            <li>
-                <b>The Agreement</b>:<br />
-                The first e-mail you receive is the Agreement itself, it contains the fees, settlement terms and what we have registed on you as a customer. This is used as our shared documentation on what the actual agreement betwen us is.
-            </li>
-            <li><b>The POA</b>:<br />
-                The second e-mail you receive is the POA. A POA is a contract giving us the authority to enable the financial agreement between you and the financial institute.<br />
-                You need to sign both e-mails before the onboarding-flow have been fully completed.
-            </li>
-        </ol>
-        <div class="col-3">
-            <button class="form-control btn btn-success col-3 sendContract" data-href="contracts_sent" data-validation="sendContracts">Send me the contract</button>
+        <div class="col-12">
+            <h2>The Contract</h2>
+            Thank you for providing all the documentation in the steps before. You are now ready to sign the financial contract with us.<br />
+            The following contract must be signed by an Authorized signer of the company.<br />
+            The contract is in two parts:
+            <ol>
+                <li>
+                    <b>The Agreement</b>:<br />
+                    The first e-mail you receive is the Agreement itself, it contains the fees, settlement terms and what we have registed on you as a customer. This is used as our shared documentation on what the actual agreement betwen us is.
+                </li>
+                <li><b>The POA</b>:<br />
+                    The second e-mail you receive is the POA. A POA is a contract giving us the authority to enable the financial agreement between you and the financial institute.<br />
+                    You need to sign both e-mails before the onboarding-flow have been fully completed.
+                </li>
+            </ol>
+        </div>
+        <div class="col-12">
+            <button class="form-control btn btn-success col-3 sendContract" data-href="contracts_sent" data-validation="sendContracts">Send the contract for signing</button>
         </div>
     </div>
     <div class="step2 row contracts_sent">
