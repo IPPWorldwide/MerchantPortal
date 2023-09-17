a7f2de93369d5601a0f1e03034a4ef6a9d40d689
Mathias Gajhede <mag@venturecapitals.dk>
2023-09-18 00:09:29 +0200

    Update contract.js

--- onboarding/assets/js/contract.js
+++ onboarding/assets/js/contract.js
@@ -3,7 +3,7 @@ function sendContracts() {
         type: 'POST',
         url: GLOBAL_BASE_URL + '/company/data/onboarding/confirm/',
         dataType: "json",
-        data: { api_key: company.api_key, company_id: company.id },
+        data: { api_key: company.api_key, company_id: company.id, portal_url: PORTAL_URL },
         success: function (response) {
             $.each( onboarding_extensions, function( key, value ) {
                 $.ajax({
