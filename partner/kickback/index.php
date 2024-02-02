fc14c6a80dff4c1e663c4bfea5180f4e29e63a10
Mathias Gajhede <mag@venturecapitals.dk>
2024-02-02 10:40:58 +0100

    Partner kickback (#188)

--- partner/kickback/index.php
+++ partner/kickback/index.php
@@ -16,8 +16,13 @@ echo '
           <thead>
             <tr>
               <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["TRANSACTION_ID"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["ORDER_ID"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["COMPANY_ID"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["COMPANY_NAME"].'</th>
               <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["ACTION_ID"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["TRANSACTION_DATE"].'</th>
               <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["CURRENCY"].'</th>
+              <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["ACTION_VALUE"].'</th>
               <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["COST"].'</th>
               <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["PROFIT_CURRENCY"].'</th>
               <th scope="col">'.$lang["PARTNER"]["KICKBACK"]["PROFIT"].'</th>
@@ -30,11 +35,16 @@ echo '
                     echo '       
                     <tr>
                       <td>'.$tnx->id.'</td>
+                      <td>'.$tnx->order_data->id.'</td>
+                      <td>'.$tnx->company->id.'</td>
+                      <td>'.$tnx->company->name.'</td>
                       <td>'.$tnx->action->id.'</td>
+                      <td>'.$tnx->action->time->readable.'</td>
                       <td>'.$tnx->currency->txt.'</td>
-                      <td>'.(($tnx->costs->acquirer+$tnx->costs->fixed)/100).'</td>
+                      <td>'.$tnx->action->value.'</td>
+                      <td>'.$tnx->costs->total.'</td>
                       <td>'.$tnx->profit->currency->txt.'</td>
-                      <td>'.($tnx->profit->value/100).'</td>
+                      <td>'.$tnx->profit->value.'</td>
                       <td>'.$tnx->settled.'</td>
                     </tr>
                     ';
@@ -45,38 +55,4 @@ echo '
         </table>
       </div>
 <?php
-echo '
-    <div class="modal fade" id="passwordModal" tabindex="-1" role="dialog" aria-labelledby="passwordModalTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLongTitle"></h5>
-                </div>
-                <div class="modal-body">
-                    <form>
-                        <input type="hidden" name="company_id" id="company-id" readonly>
-                        <input type="hidden" name="user_id" id="user-id" readonly>
-                        <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">'.$lang["PARTNER"]["COMPANIES"]["MODAL_USERNAME"].'</label>
-                            <input type="text" class="form-control"  name="username" id="username">
-                        </div>
-                        <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">'.$lang["PARTNER"]["COMPANIES"]["MODAL_PASSWORD"].'</label>
-                            <input type="password" class="form-control checkPasswordUser" name="password" id="password">
-                        </div>
-                        <div class="form-group">
-                            <label for="recipient-name" class="col-form-label">'.$lang["PARTNER"]["COMPANIES"]["MODAL_REPEAT_PASSWORD"].'</label>
-                            <input type="password" class="form-control checkPasswordUser" id="repeat-password">
-                            <small id="PasswordRequirements">'.$lang["PARTNER"]["COMPANIES"]["MODAL_PASSWORD_DESCRIPTION"].'</small>
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary closeModal">'.$lang["PARTNER"]["COMPANIES"]["MODAL_CLOSE"].'</button>
-                    <button type="button" class="btn btn-primary confirm" disabled>'.$lang["PARTNER"]["COMPANIES"]["MODAL_SUBMIT_BTN"].'</button>
-                </div>
-            </div>
-        </div>
-    </div>
-';
 echo foot();
